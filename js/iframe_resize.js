688edddb80dcad98c14e7614ec7c575d7ea2df4e
Yi Jiang <yicjiang@gmail.com>
2018-03-27 10:56:02 +1100

    Code review finxing

--- js/iframe_resize.js
+++ js/iframe_resize.js
@@ -7,17 +7,14 @@
 (function ($) {
 
   // Create IE + others compatible event handler
-  var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+  var eventMethod = window.addEventListener ? 'addEventListener' : 'attachEvent';
   var eventer = window[eventMethod];
-  var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-
-
+  var messageEvent = eventMethod === 'attachEvent' ? 'onmessage' : 'message';
 
   // Listen to message from survey
+  eventer(messageEvent,function( e ) {
 
-  eventer(messageEvent,function(e) {
-
-    console.log('QUALTRICS: Received message!:  ',e.data);
+    console.log('QUALTRICS: Received message!:  ', e.data );
 
     if(e.data=="closeQSIWindow") {
 
@@ -30,14 +27,12 @@
     } else {
 
       // Just do the Frame Expansion if pixel height is passed
-
       // $('#qualtrics-feedback-zone').contents()[0].style.height = e.data;
-      $('.qualtrics_iframe').height(e.data);
-      $('.qualtrics_iframe').width("100%");
+      $('.qualtrics_iframe').height( e.data );
+      $('.qualtrics_iframe').width( "100%" );
 
     }
 
-  },false);
-
+  }, false);
 
 })(jQuery);
--- src/qualtricsxm.php
+++ src/qualtricsxm.php
@@ -1,93 +1,96 @@
 <?php
 
-  class qualtricsxm {
+/**
+ * Helper class for Qualtrics API Call
+ */
+class Qualtricsxm {
 
-    private $api_base_url;
-    private $api_token;
+  private $apiBaseUrl;
+  private $apiToken;
 
-    public function __construct ($api_base_url, $api_token) {
-      $this->api_base_url = $api_base_url;
-      $this->api_token = $api_token;
-    }
-
-    /**
-     * API call.
-     *
-     * @param array $url_params
-     * @return object
-     */
-    public function httpRequest($url_params) {
-     $options = array(
-        'method' => 'GET',
-        'timeout' => 15,
-        'headers' => array('X-API-TOKEN' => $this->api_token),
-      );
-
-      $api_req = "/" ;
-      foreach ($url_params as $url => $val) {
-        $api_req .= urlencode($url) . "/" . urlencode($val);
-      }
-
-      $url = $this->api_base_url . $api_req;
-
-      $request = drupal_http_request($url, $options);
-      return $request;
-    }
-
-
-    /**
-     * Get survey by surveyID
-     * @param $survey_id
-     * @return bool|mixed
-     */
-    public function getSurvey ($survey_id) {
-      $survey =  $this->httpRequest(array("surveys" => $survey_id));
-
-      if ($survey->code != 200) {
-        return FALSE;
-      }
-
-      $survey_data = json_decode($survey->data);
-      return $survey_data;
-    }
-
-
-    /**
-     * Get survey list.
-     * @return bool|array
-     * TODO merge into getSurve.
-     */
-    public function getSurveyList() {
-      $survey = $this->httpRequest(array('surveys' => ''));
-
-      if ($survey->code != 200) {
-        return FALSE;
-      }
-
-      $survey_data = json_decode($survey->data);
-
-      //Make sure the legacy function working, and renderable by theme_table.
-      foreach ($survey_data->result->elements as $element) {
-        $surveys_array[$element->id]['surveyname'] = $element->name;
-        $surveys_array[$element->id]['id'] = $element->id;
-        $surveys_array[$element->id]['ownerId'] = $element->ownerId;
-        $surveys_array[$element->id]['lastModified'] = $element->lastModified;
-        $surveys_array[$element->id]['isActive'] = $element->isActive;
-      }
-      return $surveys_array;
-    }
-
-
-    /**
-     * Get extra submission meta data from API call.
-     * @param $survey_id
-     * @return mixed
-     */
-    public function getSubmissions($survey_id) {
-      $request_data = $this->getSurvey($survey_id);
-      $response_counts =  $request_data->result->responseCounts;
-      return $response_counts;
-    }
-
-
+  public function __construct ($apiBaseUrl, $apiToken) {
+    $this->api_base_url = $apiBaseUrl;
+    $this->api_token = $apiToken;
   }
+
+  /**
+   * API call.
+   *
+   * @param array $url_params
+   * @return object
+   */
+  public function httpRequest($url_params) {
+   $options = array(
+      'method' => 'GET',
+      'timeout' => 15,
+      'headers' => array('X-API-TOKEN' => $this->api_token),
+    );
+
+    $api_req = "/" ;
+    foreach ($url_params as $url => $val) {
+      $api_req .= urlencode($url) . "/" . urlencode($val);
+    }
+
+    $url = $this->api_base_url . $api_req;
+
+    $request = drupal_http_request($url, $options);
+    return $request;
+  }
+
+  /**
+   * Get survey by surveyID
+   * 0@param $survey_id
+   * @return bool|mixed
+   */
+  public function getSurvey ($survey_id) {
+    $survey =  $this->httpRequest(array("surveys" => $survey_id));
+
+    if ($survey->code != 200) {
+      return FALSE;
+    }
+
+    $survey_data = json_decode($survey->data);
+    return $survey_data;
+  }
+
+  /**
+   * Get survey list.
+   * @return bool|array
+   * TODO merge into getSurve.
+   */
+  public function getSurveyList() {
+    $survey = $this->httpRequest(array('surveys' => ''));
+
+    if ($survey->code != 200) {
+      return FALSE;
+    }
+
+    $survey_data = json_decode($survey->data);
+
+    // Make sure the legacy function working, and renderable by theme_table.
+    foreach ($survey_data->result->elements as $element) {
+      $surveys_array[$element->id]['surveyname'] = $element->name;
+      $surveys_array[$element->id]['id'] = $element->id;
+      $surveys_array[$element->id]['ownerId'] = $element->ownerId;
+      $surveys_array[$element->id]['lastModified'] = $element->lastModified;
+      $surveys_array[$element->id]['isActive'] = $element->isActive;
+    }
+    return $surveys_array;
+  }
+
+  /**
+   * Get extra submission meta data from API call.
+   * @param $survey_id
+   * @return mixed
+   */
+  public function getSubmissions($survey_id) {
+    $request_data = $this->getSurvey($survey_id);
+
+    if(!$request_data) {
+      return false;
+    }
+    $response_counts =  $request_data->result->responseCounts;
+    return $response_counts;
+  }
+
+}
